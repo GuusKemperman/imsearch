e0024dfdd9848ececa1b466fc9899623968b6b2d
GuusKemperman <guus.kemperman@gmail.com>
2025-04-16 14:12:25 +0200

    Fixed missing function

--- .github/example_imsearch.cpp
+++ .github/example_imsearch.cpp
@@ -3,15 +3,14 @@
 
 #include "imgui.h"
 #include "imsearch.h"
-#include "stdio.h"
+#include <cstdio>
 
 int main(int, char**)
 {    
-    printf("sample_implot: start\n");
+    puts("sample_imsearch: start\n");
 
     IMGUI_CHECKVERSION();
     ImGui::CreateContext();
-    ImPlot::CreateContext();
 
     // Additional imgui initialization needed when no backend is present
     ImGui::GetIO().DisplaySize = ImVec2(400.f, 400.f);
@@ -21,35 +20,11 @@ int main(int, char**)
     for(int counter = 0; counter < 500; ++counter)
     {
         ImGui::NewFrame();
-
-        if (ImGui::Begin("Hello, world!"))
-        {
-            ImGui::Text("Hello again");
-
-            if (ImPlot::BeginPlot("My Plot"))
-            {
-                static double values[] = {1., 3., 5.};
-                ImPlot::PlotLine("Values", values, 3);
-                ImPlot::EndPlot();
-            }
-
-            #ifdef IMPLOT_INSTANTIATE_ALL_NUMERIC_TYPES
-            if (ImPlot::BeginPlot("My Plot (long double)"))
-            {
-                static long double values[] = {1., 3., 5.};
-                ImPlot::PlotLine("Values", values, 3);
-                ImPlot::EndPlot();
-            }
-            #endif
-
-            ImGui::End();
-        }
-
+        ImSearch::ShowDemoWindow();
         ImGui::Render();
     }
 
-    ImPlot::DestroyContext();
     ImGui::DestroyContext();
-    printf("sample_implot: end\n");
+    puts("sample_imsearch: end\n");
     return 0;
 }
