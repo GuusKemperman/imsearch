2509b4a41a3cbc213201e2f39150d082b9bf507e
GuusKemperman <guus.kemperman@gmail.com>
2025-04-27 20:02:36 +0200

    Refactored and cleaned up

--- imsearch_internal.h
+++ imsearch_internal.h
@@ -1,15 +1,39 @@
 #pragma once
 #ifndef IMGUI_DISABLE
+#include <imgui.h>
+
 #include <cstddef>
 #include <cstdint>
-#include <imgui.h>
+#include <cctype>
+
 #include <string>
 #include <vector>
+#include <memory>
+#include <string>
+#include <stack>
+#include <algorithm>
+#include <unordered_map>
+#include <type_traits>
+#include <numeric>
+#include <limits>
 
 namespace ImSearch
 {
 	using IndexT = std::uint32_t;
+
+
+	//-----------------------------------------------------------------------------
+	// [SECTION] Constants
+	//-----------------------------------------------------------------------------
+
+	// Anything below this score is not displayed to the user.
+	constexpr float sCutOffStrength = .5f;
+
 	constexpr IndexT sNullIndex = std::numeric_limits<IndexT>::max();
+
+	//-----------------------------------------------------------------------------
+	// [SECTION] Structs
+	//-----------------------------------------------------------------------------
 
 	struct StrView
 	{
@@ -21,6 +45,8 @@ namespace ImSearch
 			mData(data),
 			mSize(size) {}
 
+		const char* begin() const { return mData; }
+		const char* end() const { return mData + mSize; }
 		const char* data() const { return mData; }
 		IndexT size() const { return mSize; }
 
@@ -30,22 +56,150 @@ namespace ImSearch
 		IndexT mSize{};
 	};
 
-	namespace Internal
+	// Here is why we are using VTables instead of just std::function:
+	// std::function's SBO optimisation means it might consume
+	// more memory than needed if the user has not captured anything,
+	// and if the user is capturing more than fits in the SBO storage,
+	// performance tanks due to the many heap allocations. Now,
+	// the Callback is slower than using std::function, it makes
+	// the implementation and API slightly more complicated, BUT, if we
+	// were to release with std::function in the public API, I would not
+	// be able to get rid of std::function without breaking forward
+	// compatibility in the future. By keeping the concept of how functors
+	// are stored abstracted away in the backend, it can be optimised in
+	// the future without breaking API.
+	struct Callback
 	{
-		//-----------------------------------------------------------------------------
-		// [SECTION] Testing
-		//-----------------------------------------------------------------------------
+		Callback() = default;
 
-		float GetScore(size_t index);
+		Callback(void* originalFunctor, ImSearch::Internal::VTable vTable);
 
-		size_t GetDisplayOrderEntry(size_t index);
-	}
+		Callback(const Callback&) = delete;
+		Callback(Callback&& other) noexcept;
+
+		Callback& operator=(const Callback&) = delete;
+		Callback& operator=(Callback&& other) noexcept;
+
+		~Callback();
+
+		operator bool() const { return mData != nullptr; }
+
+		// PushSearchable
+		bool operator()(const char* name) const;
+
+		// PopSearchable
+		void operator()() const;
+
+		void ClearData();
+
+		enum VTableModes
+		{
+			Invoke = 0,
+			MoveConstruct = 1,
+			Destruct = 2,
+			GetSize = 3
+		};
+
+		ImSearch::Internal::VTable mVTable{};
+		char* mData{};
+	};
+
+	struct Searchable
+	{
+		std::string mText{};
+
+		IndexT mIndexOfFirstChild = sNullIndex;
+		IndexT mIndexOfLastChild = sNullIndex;
+		IndexT mIndexOfParent = sNullIndex;
+		IndexT mIndexOfNextSibling = sNullIndex;
+	};
+
+	struct Input
+	{
+		std::vector<Searchable> mEntries{};
+		std::vector<float> mBonuses{};
+		std::string mUserQuery{};
+	};
 
 	struct ReusableBuffers
 	{
 		std::vector<float> mScores{};
 		std::vector<IndexT> mTempIndices{};
 	};
+
+	struct Output
+	{
+		std::vector<IndexT> mDisplayOrder{};
+		static constexpr IndexT sDisplayEndFlag = static_cast<IndexT>(1) << static_cast<IndexT>(std::numeric_limits<IndexT>::digits - 1);
+	};
+
+	struct Result
+	{
+		Input mInput{};
+		ReusableBuffers mBuffers{};
+		Output mOutput{};
+	};
+
+	struct DisplayCallbacks
+	{
+		Callback mOnDisplayStart{};
+		Callback mOnDisplayEnd{};
+	};
+
+	struct LocalContext
+	{
+		Input mInput{};
+
+		std::vector<DisplayCallbacks> mDisplayCallbacks{};
+
+		std::stack<IndexT> mPushStack{};
+		Result mResult{};
+		bool mHasSubmitted{};
+	};
+
+	struct ImSearchContext
+	{
+		std::unordered_map<ImGuiID, LocalContext> Contexts{};
+		std::stack<std::reference_wrapper<LocalContext>> ContextStack{};
+		std::unordered_map<std::string, std::string> mTokenisedStrings{};
+	};
+
+	bool operator==(const Searchable& lhs, const Searchable& rhs);
+	bool operator==(const Input& lhs, const Input& rhs);
+
+	//-----------------------------------------------------------------------------
+	// [SECTION] Context
+	//-----------------------------------------------------------------------------
+
+	// Some helper functions for generalising error-reporting.
+	LocalContext& GetLocalContext();
+	ImSearch::ImSearchContext& GetImSearchContext();
+	IndexT GetCurrentItem(LocalContext& context);
+	
+	// Is the context currently collecting submissions?
+	// ImSearch does not store anything the programm is submitting if the user
+	// is not actively searching, for performance and memory reasons.
+	bool CanCollectSubmissions();
+
+	//-----------------------------------------------------------------------------
+	// [SECTION] Testing
+	//-----------------------------------------------------------------------------
+
+	float GetScore(size_t index);
+
+	size_t GetDisplayOrderEntry(size_t index);
+	
+	//-----------------------------------------------------------------------------
+	// [SECTION] Fuzzy Searching & String Functions
+	//-----------------------------------------------------------------------------
+
+	std::vector<std::string> SplitTokens(StrView s);
+
+	std::string Join(const std::vector<std::string>& tokens);
+
+	std::string MakeTokenisedString(StrView original);
+
+	StrView GetMemoizedTokenisedString(const std::string& original);
 
 	int LevenshteinDistance(
 		StrView s1,
@@ -60,8 +214,11 @@ namespace ImSearch
 		StrView s2,
 		ReusableBuffers& buffers);
 
-	float WRatio(StrView s1,
+	// The function used internally to score strings
+	float WeightedRatio(StrView s1,
+		StrView s1Tokenised,
 		StrView s2,
+		StrView s2Tokenised,
 		ReusableBuffers& buffers);
 }
 
